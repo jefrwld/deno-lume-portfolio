8cbb4b0f823257e08e42b926cf5c94e5419b385f
jeffrey <jeffrey.freiwald98@gmail.com>
2024-11-12 19:08:14 +0100

    addedo image and styled a little bit the about me section

--- _config.ts
+++ _config.ts
@@ -1,8 +1,11 @@
 import lume from "lume/mod.ts";
 import prism from "https://deno.land/x/lume/plugins/prism.ts";
+import picture from "lume/plugins/picture.ts";
+import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume();
-
+site.use(picture(/* Options */));
+site.use(transformImages());
 site.copy("/styles.css");
 site.copy("/prism.css");
 
