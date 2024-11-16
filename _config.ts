10e21490107488ebc1212776f75860c705effc0e
jeffrey <jeffrey.freiwald98@gmail.com>
2024-11-16 14:00:20 +0100

    testing with urls

--- _config.ts
+++ _config.ts
@@ -3,6 +3,9 @@ import prism from "https://deno.land/x/lume/plugins/prism.ts";
 import picture from "lume/plugins/picture.ts";
 import transformImages from "lume/plugins/transform_images.ts";
 
+
+  
+
 const site = lume();
 site.use(picture(/* Options */));
 site.use(transformImages());
@@ -13,5 +16,8 @@ site.copy("/004.jpg");
 
 site.use(prism());
 
-export default site;
+export default {
+    site,
+    base: "/deno-lume-portfolio/",
+  };
 
