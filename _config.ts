cceac8c46a248acd8a7f1c722a57f008aadaec5a
jeffrey <jeffrey.freiwald98@gmail.com>
2024-11-16 13:17:56 +0100

    add workflow

--- _config.ts
+++ _config.ts
@@ -1,5 +1,4 @@
-import lume from "https://deno.land/x/lume/mod.ts";
-
+import lume from "lume/mod.ts";
 import prism from "https://deno.land/x/lume/plugins/prism.ts";
 import picture from "lume/plugins/picture.ts";
 import transformImages from "lume/plugins/transform_images.ts";
